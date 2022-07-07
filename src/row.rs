8c1792356903a69f2210c331a529d86d24e3f40c
jeff2857 <jeffliu2857@gmail.com>
2022-07-07 10:50:10 +0800

    feat: tackle grapheme

--- src/row.rs
+++ src/row.rs
@@ -1,28 +1,45 @@
+use unicode_segmentation::UnicodeSegmentation;
+
 pub struct Row {
     string: String,
+    len: usize,
 }
 
 impl Row {
     pub fn render(&self, start: usize, end: usize) -> String {
         let end = std::cmp::min(end, self.string.len());
         let start = std::cmp::min(start, end);
-        self.string.get(start..end).unwrap_or_default().to_string()
+
+        let mut result = String::new();
+        for grapheme in self.string[..].graphemes(true).skip(start).take(end - start) {
+            result.push_str(grapheme);
+        }
+
+        result
     }
 
     pub fn len(&self) -> usize {
-        self.string.len()
+        self.len
     }
 
     pub fn is_empty(&self) -> bool {
-        self.string.is_empty()
+        self.len == 0
+    }
+
+    pub fn update_len(&mut self) {
+        self.len = self.string[..].graphemes(true).count();
     }
 }
 
 impl From<&str> for Row {
     fn from(slice: &str) -> Self {
-        Self {
+        let mut row = Self {
             string: String::from(slice),
-        }
+            len: 0,
+        };
+        row.update_len();
+
+        row
     }
 }
 
